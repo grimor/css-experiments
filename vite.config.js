03b78a92ec5d14a614a3d34366abc7f1a615ff51
Mikołaj Stolarski <grimor@users.noreply.github.com>
2026-07-14 00:42:46 +0200

    Update rollup input configuration for new examples

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,10 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        example_1: resolve(__dirname, "example_1/index.html"),
+        "card-stack": resolve(__dirname, "card-stack/index.html"),
+        "counter": resolve(__dirname, "counter/index.html"),
+        "repeller": resolve(__dirname, "repeller/index.html"),
+        "spotlight": resolve(__dirname, "spotlight/index.html"),
       },
     },
   },
